850c23c92f4bce2620f8c2d6ddb8d4caf53343a2
Tom Hoglund <infrapale@gmail.com>
2019-06-30 20:15:18 +0300

    Update LoRaDriveTest.py

--- LoRaDriveTest.py
+++ LoRaDriveTest.py
@@ -1,6 +1,6 @@
 """
 Example for using the RFM9x Radio with Raspberry Pi.
-
+Just testing Git
 Learn Guide: https://learn.adafruit.com/lora-and-lorawan-for-raspberry-pi
 Author: Brent Rubell for Adafruit Industries
 """
